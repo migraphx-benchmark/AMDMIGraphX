a08cd3655953c0f82c916bb2362acd57fab588a8
Gyula Zakor <gyula.zakor@htecgroup.com>
2023-10-03 13:33:00 +0000

    Add support for IsInf ONNX operator

--- test/onnx/verify_onnx.cpp
+++ test/onnx/verify_onnx.cpp
@@ -1014,6 +1014,82 @@ TEST_CASE(instance_norm_3d_test)
     EXPECT(migraphx::verify::verify_rms_range(result_vector, gold));
 }
 
+TEST_CASE(isinf_half_test)
+{
+    migraphx::program p = migraphx::parse_onnx("isinf_half_test.onnx");
+    p.compile(migraphx::make_target("ref"));
+
+    migraphx::shape s{migraphx::shape::half_type, {2, 3}};
+    migraphx::parameter_map pp;
+    std::vector<float> tmp = {-1.7, NAN, INFINITY, 3.6, -INFINITY, INFINITY};
+    std::vector<migraphx::half> data{tmp.cbegin(), tmp.cend()};
+    pp["t1"] = migraphx::argument(s, data.data());
+
+    auto result = p.eval(pp).back();
+    std::vector<float> result_vector;
+    result.visit([&](auto output) { result_vector.assign(output.begin(), output.end()); });
+
+    std::vector<float> gold = {0, 0, 1, 0, 1, 1};
+    EXPECT(migraphx::verify::verify_rms_range(result_vector, gold));
+}
+
+TEST_CASE(isinf_half_neg_test)
+{
+    migraphx::program p = migraphx::parse_onnx("isinf_half_neg_test.onnx");
+    p.compile(migraphx::make_target("ref"));
+
+    migraphx::shape s{migraphx::shape::half_type, {2, 3}};
+    migraphx::parameter_map pp;
+    std::vector<float> tmp = {-1.7, NAN, INFINITY, 3.6, -INFINITY, INFINITY};
+    std::vector<migraphx::half> data{tmp.cbegin(), tmp.cend()};
+    pp["t1"] = migraphx::argument(s, data.data());
+
+    auto result = p.eval(pp).back();
+    std::vector<float> result_vector;
+    result.visit([&](auto output) { result_vector.assign(output.begin(), output.end()); });
+
+    std::vector<float> gold = {0, 0, 0, 0, 1, 0};
+    EXPECT(migraphx::verify::verify_rms_range(result_vector, gold));
+}
+
+TEST_CASE(isinf_half_pos_test)
+{
+    migraphx::program p = migraphx::parse_onnx("isinf_half_pos_test.onnx");
+    p.compile(migraphx::make_target("ref"));
+
+    migraphx::shape s{migraphx::shape::half_type, {2, 3}};
+    migraphx::parameter_map pp;
+    std::vector<float> tmp = {-1.7, NAN, INFINITY, 3.6, -INFINITY, INFINITY};
+    std::vector<migraphx::half> data{tmp.cbegin(), tmp.cend()};
+    pp["t1"] = migraphx::argument(s, data.data());
+
+    auto result = p.eval(pp).back();
+    std::vector<float> result_vector;
+    result.visit([&](auto output) { result_vector.assign(output.begin(), output.end()); });
+
+    std::vector<float> gold = {0, 0, 1, 0, 0, 1};
+    EXPECT(migraphx::verify::verify_rms_range(result_vector, gold));
+}
+
+TEST_CASE(isinf_no_detect_test)
+{
+    migraphx::program p = migraphx::parse_onnx("isinf_no_detect_test.onnx");
+    p.compile(migraphx::make_target("ref"));
+
+    migraphx::shape s{migraphx::shape::float_type, {2, 3}};
+    migraphx::parameter_map pp;
+    std::vector<float> tmp = {-1.7, NAN, INFINITY, 3.6, -INFINITY, INFINITY};
+    std::vector<migraphx::half> data{tmp.cbegin(), tmp.cend()};
+    pp["t1"] = migraphx::argument(s, data.data());
+
+    auto result = p.eval(pp).back();
+    std::vector<float> result_vector;
+    result.visit([&](auto output) { result_vector.assign(output.begin(), output.end()); });
+
+    std::vector<float> gold = {0, 0, 0, 0, 0, 0};
+    EXPECT(migraphx::verify::verify_rms_range(result_vector, gold));
+}
+
 TEST_CASE(layer_norm_test)
 {
     std::vector<float> scale{1.2, 0.8};
